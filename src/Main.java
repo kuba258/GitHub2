287e220979221a3fdc70635b68c6b4ca27adb268
Kuba <micek.kuba@gmail.com>
2015-09-06 14:29:56 +0200

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.TreeSet;
+
 /**
  * Created by Kuba on 06.09.15.
  */
 public class Main {
+
+    static TreeSet tree = new TreeSet();
+
     public static void main(String[] args){
+    tree.add("Ala");
 
         System.out.println("Hello");
 
